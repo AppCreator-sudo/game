7714c01490d510068646edca1e04459f915ed04d
Admin <orisio90@gmail.com>
2024-08-31 19:56:24 +0700

    добавлено поп-ап при нажатии

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+    const handleButtonClick = () => {
+        setIsPopupVisible(true);
+        setTimeout(() => {
+            setIsPopupVisible(false);
+        }, 2000); // Скрыть всплывающее окно через 2 секунды
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src="lottery-button.png" alt="Lottery Button" className="lottery-image" />
+                <img src="lottery-button.png" alt="Lottery Button" className="lottery-image" onClick={handleButtonClick} />
+                <div className={isPopupVisible ? 'popup popup-show' : 'popup'}>Попробуй еще раз</div>
             </header>
         </div>
     );
