90d7ba1b0ee029b82bd54112e5a0602f1746a846
Admin <orisio90@gmail.com>
2024-08-31 19:24:15 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="lottery-button.png" alt="Lottery Button" className="lottery-image" />
+            </header>
+        </div>
+    );
+}
+
+export default App;
