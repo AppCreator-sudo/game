d56be865007a735c6ff32a661fd6c323e648fe82
Admin <orisio90@gmail.com>
2024-09-01 18:05:05 +0700

    исправление кнопки перевода на 0.02 TON

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [isPopupVisible, setIsPopupVisible] = useState(false);
 
-    const handleButtonClick = () => {
-        setIsPopupVisible(true);
-        setTimeout(() => {
-            setIsPopupVisible(false);
-        }, 2000); // Скрыть всплывающее окно через 2 секунды
-    };
 
     return (
         <div className="App">
             <header className="App-header">
-                <img src="lottery-button.png" alt="Lottery Button" className="lottery-image" onClick={handleButtonClick} />
+                <img src="lottery-button.png" alt="Lottery Button" className="lottery-image" />
 
             </header>
         </div>
