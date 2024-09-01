bf3583350d241e2be53fb96f5fe377ff86dc7bd3
Admin <orisio90@gmail.com>
2024-09-01 21:08:53 +0700

    внешняя правка, добавление джекпота, корректировка кнопок

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import React, { useState } from 'react';
+//import React from 'react';
 import './App.css';
 
 function App() {
-
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src="lottery-button.png" alt="Lottery Button" className="lottery-image" />
-
-            </header>
-        </div>
-    );
 }
-
 export default App;
